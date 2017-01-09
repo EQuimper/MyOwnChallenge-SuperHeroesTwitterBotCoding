f6f78ba8f90b853fbc606c604d73384ac1294d64
EQuimper <quimperemanuel@gmail.com>
2017-01-09 01:15:16 -0500

    Create the basic bot

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,17 @@
 import Twit from 'twit';
+import jsonfile from 'jsonfile';
 import config from './config';
+import { quotes } from './quotes';
+import { getRandomQuotes } from './helpers';
 
+const phraseToLook = ['#100DaysOfCode'];
+const blackListUser = ['_100DaysOfCode', 'heroes_bot'];
 const { consumer_key, consumer_secret, access_token, access_token_secret } = config;
 
-var T = new Twit({
+/*
+* INIT THE BOT
+*/
+const T = new Twit({
   consumer_key,
   consumer_secret,
   access_token,
@@ -12,3 +20,44 @@ var T = new Twit({
 });
 
 console.log('Bot is running...');
+
+const getMess = name => {
+  let mess;
+  const qt = getRandomQuotes(quotes);
+  mess = `@${name} • "${qt.text}" -${qt.author} #goodJob #coderBot`;
+
+  if (mess.length > 144) {
+    getMess(name);
+  } else {
+    return mess;
+  }
+}
+
+/*
+* TWEET function
+*/
+const tweetIt = name => {
+  const tweet = {
+    status: getMess(name)
+  };
+  T.post('statuses/update', tweet, (err, data, res) => {
+    console.log({ err, data, res });
+  });
+};
+
+/*
+* GET LIVE UPDATE WITH THE HASHTAG WE SEARCH
+*/
+const stream = T.stream('statuses/filter', { track: phraseToLook })
+
+stream.on('tweet', t => {
+  console.log('NEW TWEET');
+
+  // Be sure we don't retweet a bot
+  if (blackListUser.includes(t.user.screen_name)) {
+    console.log('USER BLOCK', t.user.screen_name);
+    return;
+  }
+
+  tweetIt(t.user.screen_name);
+});
